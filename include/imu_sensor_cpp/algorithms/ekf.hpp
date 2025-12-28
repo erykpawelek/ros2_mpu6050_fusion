e1633d4e9e9c880d8a20d999bf0241059a2762ad
erykpawelek <panzereryk03@gmail.com>
2025-12-28 18:03:35 +0000

    feat(ekf) Extended Kalman Filter restored after crash

--- include/imu_sensor_cpp/algorithms/ekf.hpp
+++ include/imu_sensor_cpp/algorithms/ekf.hpp
@@ -7,33 +7,92 @@
 
 namespace ekf
 {
-
+/**
+* @class ExtendedKalmanFilter
+* @brief Implementation of the Extended Kalman Filter (EKF) for AHRS orientation estimation.
+*
+* This class estimates the 3D orientation of an object represented by a quaternion.
+* It uses gyroscope data for prediction (process model) and accelerometer data
+* for correction (measurement model).
+*
+* @details
+* The filter operates on a 4x1 state vector (Quaternion).
+* - Prediction step: Integrates angular velocity.
+* - Correction step: Corrects drift using the gravity vector.
+*/
 class ExtendedKalmanFilter{
 public:
-
+    /**
+    * @brief State Vector [4x1].
+    * Represents the orientation quaternion q = [qw, qx, qy, qz]^T.
+    * qw - scalar part, qx,qy,qz - vector part.
+    */
     using StateVector = Eigen::Matrix<double, 4, 1>;
 
+    /**
+    * @brief State/Covariance Matrix [4x4].
+    * Used for the Error Covariance Matrix (P) and Process Noise Covariance (Q).
+    */
     using StateMatrix = Eigen::Matrix<double, 4, 4>;
 
+    /**
+    * @brief Measurement Matrix [3x3].
+    * Used for the Measurement Noise Covariance (R).
+    */
     using MeasurementMatrix = Eigen::Matrix<double, 3, 3>;
 
+    /**
+    * @brief Constructor for the Extended Kalman Filter.
+    * Initializes covariance matrices and noise parameters.
+    * @param R_noise Measurement Noise Covariance Matrix (3x3). 
+    * @param process_noise_variance Variance scalar for process noise.
+    */
     ExtendedKalmanFilter(
-        const MeasurementMatrix& R_noise,
+        const MeasurementMatrix& R_noise, 
         double process_noise_variance);
 
+    /**
+    * @brief Initializes the state using the first accelerometer reading.
+    *
+    * Calculates initial Roll and Pitch angles based on the gravity vector
+    * (assuming the robot is stationary) and converts them to the initial Quaternion.
+    * Yaw is initialized to 0.0.
+    *
+    * @param imu_data Structure containing raw IMU data (accelerometer).
+    */
     void init(const mpu6050cust_driver::MPU6050CustomDriver<mpu6050cust_driver::LinuxI2C>::ImuData & imu_data);
+
+    void predict(const mpu6050cust_driver::MPU6050CustomDriver<mpu6050cust_driver::LinuxI2C>::ImuData & imu_data, double dt);
+
+    void update(const mpu6050cust_driver::MPU6050CustomDriver<mpu6050cust_driver::LinuxI2C>::ImuData & imu_data);
 
 private:
 
+    /**
+    * @brief Current State Vector (Quaternion).
+    * x = [qw, qx, qy, qz]^T
+    */
     StateVector x_;
 
+    /**
+    * @brief Process Noise Covariance Matrix.
+    * Represents the uncertainty of the physical model (gyro integration).
+    * Constant in this implementation.
+    */
     StateMatrix Q_;
 
+    /**
+    * @brief Error Covariance Matrix.
+    * Represents the current estimation uncertainty.
+    * This matrix "breathes": grows during prediction, shrinks during correction.
+    */
     StateMatrix P_;
 
+    /**
+    * @brief Measurement Noise Covariance Matrix.
+    * Represents the uncertainty of the sensor (accelerometer).
+    */
     MeasurementMatrix R_;
-
 };
-
 }
 #endif 
--- src/algorithms/ekf.cpp
+++ src/algorithms/ekf.cpp
@@ -16,6 +16,72 @@ namespace ekf
         P_.setIdentity();
     }
 
-    ExtendedKalmanFilter::init(
-        const mpu6050cust_driver::MPU6050CustomDriver<mpu6050cust_driver::LinuxI2C>::ImuData & imu_data);
-}
+    void ExtendedKalmanFilter::init(const mpu6050cust_driver::MPU6050CustomDriver<mpu6050cust_driver::LinuxI2C>::ImuData & imu_data)
+    {
+        // Calculating initial state prediction
+        double roll = std::atan2(imu_data.accel_y, imu_data.accel_z);
+        double pitch = std::atan2(-imu_data.accel_x, std::sqrt(imu_data.accel_y * imu_data.accel_y +
+                                                               imu_data.accel_z * imu_data.accel_z));
+        double yaw = 0.0;
+
+        // Calculating quaternion represetation of initial prediction using eigen tools
+        Eigen::AngleAxisd rollAngle(roll, Eigen::Vector3d::UnitX());
+        Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitY());
+        Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitZ());
+
+        Eigen::Quaternion q = yawAngle * pitchAngle * rollAngle;
+
+        // Injecting initial quaternion into state vector class member variable
+        x_ << q.w(), q.x(), q.y(), q.z();
+    }
+
+    void ExtendedKalmanFilter::predict(const mpu6050cust_driver::MPU6050CustomDriver<mpu6050cust_driver::LinuxI2C>::ImuData & imu_data, double dt)
+    {
+        // Refactoring state format for quaternion operations
+        Eigen::Quaterniond q_prev(x_(0), x_(1), x_(2), x_(3));
+        // Angular velocity quaternion
+        Eigen::Quaterniond q_omega(0.0, imu_data.gyro_x, imu_data.gyro_y, imu_data.gyro_z);
+        // Quaternion derivative based ona angular velocity
+        Eigen::Quaterniond q_dot = q_prev * q_omega;
+        // New quaternion prediction
+        Eigen::Quaterniond q_new;
+        // Integrating to get new orientation and normalizing
+        q_new.coeffs() = q_prev.coeffs() + 0.5 * q_dot.coeffs() * dt;
+        q_new.normalize();
+        // Updating state vector
+        x_ << q_new.w(), q_new.x(), q_new.y(), q_new.z();
+    }
+
+    void ExtendedKalmanFilter::update(const mpu6050cust_driver::MPU6050CustomDriver<mpu6050cust_driver::LinuxI2C>::ImuData & imu_data)
+    {
+        auto GRAVITY_VECTOR = Eigen::Vector3d(0.0, 0.0, 1.0);
+
+        // Refactoring state format for quaternion operations
+        Eigen::Quaterniond q_prev(x_(0), x_(1), x_(2), x_(3));
+        // Measurement vector from accelerometer
+        Eigen::Vector3d z_meas;
+        z_meas << imu_data.accel_x, imu_data.accel_y, imu_data.accel_z;
+        z_meas.normalize();
+        // Predicted measurement based on current state
+        Eigen::Vector3d h_x = q_prev.inverse() * GRAVITY_VECTOR;
+        // Innovation calculation
+        Eigen::Vector3d innovation = z_meas - h_x;
+        
+        Eigen::Matrix<double, 3, 4> H;
+
+        H(0, 0) = -2 * q_prev.y();
+        H(0, 1) =  2 * q_prev.z();
+        H(0, 2) = -2 * q_prev.w();
+        H(0, 3) =  2 * q_prev.x();
+
+        H(1, 0) =  2 * q_prev.x();
+        H(1, 1) =  2 * q_prev.w();
+        H(1, 2) =  2 * q_prev.z();
+        H(1, 3) =  2 * q_prev.y();
+
+        H(2, 0) =  0.0;
+        H(2, 1) = -4 * q_prev.x();
+        H(2, 2) = -4 * q_prev.y();
+        H(2, 3) =  0.0;
+    }
+} // namespace ekf
