16d1f113ca62992b3183f5603397c97893c5c0b4
erykpawelek <panzereryk03@gmail.com>
2025-12-29 09:45:21 +0000

    feat(ekf): Ekf algorithm rev0 finished

--- src/algorithms/ekf.cpp
+++ src/algorithms/ekf.cpp
@@ -39,17 +39,50 @@ namespace ekf
     {
         // Refactoring state format for quaternion operations
         Eigen::Quaterniond q_prev(x_(0), x_(1), x_(2), x_(3));
+
         // Angular velocity quaternion
         Eigen::Quaterniond q_omega(0.0, imu_data.gyro_x, imu_data.gyro_y, imu_data.gyro_z);
+
         // Quaternion derivative based ona angular velocity
         Eigen::Quaterniond q_dot = q_prev * q_omega;
+        
         // New quaternion prediction
         Eigen::Quaterniond q_new;
+
         // Integrating to get new orientation and normalizing
         q_new.coeffs() = q_prev.coeffs() + 0.5 * q_dot.coeffs() * dt;
         q_new.normalize();
         // Updating state vector
         x_ << q_new.w(), q_new.x(), q_new.y(), q_new.z();
+
+        // Calculating omega matrix
+        Eigen::Matrix4d omega;
+        omega(0,0) =  0.0;
+        omega(0,1) = -imu_data.gyro_x;
+        omega(0,2) = -imu_data.gyro_y;
+        omega(0,3) = -imu_data.gyro_z;
+
+        omega(1,0) =  imu_data.gyro_x;
+        omega(1,1) =  0.0;
+        omega(1,2) =  imu_data.gyro_z;
+        omega(1,3) = -imu_data.gyro_y;
+
+        omega(2,0) =  imu_data.gyro_y;
+        omega(2,1) = -imu_data.gyro_z;
+        omega(2,2) =  0.0;
+        omega(2,3) =  imu_data.gyro_x;
+
+        omega(3,0) =  imu_data.gyro_z;
+        omega(3,1) =  imu_data.gyro_y;
+        omega(3,2) = -imu_data.gyro_x;
+        omega(3,3) =  0.0;
+
+        // Calculating transition matrix F
+        Eigen::Matrix4d F = Eigen::Matrix4d::Identity() + (0.5 * dt * omega);
+
+        // Calculating new value of error covariance matrix P
+        P_ = F * P_ * F.transpose() + Q_;
+
     }
 
     void ExtendedKalmanFilter::update(const mpu6050cust_driver::MPU6050CustomDriver<mpu6050cust_driver::LinuxI2C>::ImuData & imu_data)
@@ -58,15 +91,19 @@ namespace ekf
 
         // Refactoring state format for quaternion operations
         Eigen::Quaterniond q_prev(x_(0), x_(1), x_(2), x_(3));
+
         // Measurement vector from accelerometer
         Eigen::Vector3d z_meas;
         z_meas << imu_data.accel_x, imu_data.accel_y, imu_data.accel_z;
         z_meas.normalize();
+
         // Predicted measurement based on current state
         Eigen::Vector3d h_x = q_prev.inverse() * GRAVITY_VECTOR;
+
         // Innovation calculation
         Eigen::Vector3d innovation = z_meas - h_x;
         
+        // Jacobian matrix calculation
         Eigen::Matrix<double, 3, 4> H;
 
         H(0, 0) = -2 * q_prev.y();
@@ -83,5 +120,18 @@ namespace ekf
         H(2, 1) = -4 * q_prev.x();
         H(2, 2) = -4 * q_prev.y();
         H(2, 3) =  0.0;
+
+        // Calculating covariance of innovation
+        Eigen::Matrix3d S = H * P_ * H.transpose() + R_;
+
+        // Kalman Gain calculation, using dynamic matrix type to fit 4x3 dims
+        Eigen::MatrixXd K = P_ * H.transpose() * S.inverse();
+
+        // Calculating new state estimate
+        x_ = x_ + (K * innovation);
+        x_.normalize();
+
+        // Calculating new error covariance matrix
+        P_ = (Eigen::Matrix4d::Identity() - K * H) * P_;
     }
 } // namespace ekf
